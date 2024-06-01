ed8f0dcb6c4c1de015d750b9431ccb3da1d26bbf
sravankumar-samala <sravankumarsamala20@gmail.com>
2024-06-01 18:43:54 +0530

    Updated cors

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const userRoutes = require('./routes/userRoutes')
 
 const app = express()
 dotenv.config()
+app.use(cors())
 app.use(bodyParser.json())
+app.use('/', userRoutes)
 
 const port = process.env.PORT || 3004
 
@@ -14,6 +17,5 @@ mongoose.connect(process.env.MONGO_URI, { dbName: 'users_db' })
     .then(() => console.log('Connected Successfully'))
     .catch((error) => console.log('Error', error))
 
-app.use('/', userRoutes)
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
